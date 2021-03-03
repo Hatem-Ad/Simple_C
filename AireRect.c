69de0dabafd9a20466ff5ef1a7813ce173c46ca6
Hatem-Ad <45299723+Hatem-Ad@users.noreply.github.com>
2021-03-03 07:47:32 +0100

    Create AireRect.c

---
+++ AireRect.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+double airRect(double largeur , double hauteur)
+{
+    return largeur * hauteur;
+}
+
+int main(int argc, char *argv[])
+{
+    double largeur, hauteur;
+    printf("Donner le largeur : ");
+    scanf("%lf", &largeur);
+    printf("Donner l'hauteur : ");
+    scanf("%lf",&hauteur);
+    printf("L'aire de Ractangle est : %lf\n", airRect(largeur, hauteur));
+}
