eee740d27601758da2eb71ccce063ede9e6ac8ae
Hatem-Ad <45299723+Hatem-Ad@users.noreply.github.com>
2021-03-03 22:08:05 +0100

    Update Plus_Mois.c

--- Plus_Mois.c
+++ Plus_Mois.c
@@ -4,28 +4,50 @@
 
 int main ( int argc , char** argv )
 {
-    int nombreMystere = 0, nombreEntre = 0;
-    const int Max = 100, Min = 1;
-
-    srand(time(NULL));
-    nombreMystere = (rand() % (Max - Min + 1)) + Min;
+    int nombreMystere = 0, nombreEntre = 0,Max,Min = 1,Niveau,compteur = 0;
+    char c;
     do
     {
-        printf("Quel est le nombre :");
-        scanf("%d\n", &nombreEntre);
+        printf("Donner le niveau \n1 : facile \n2 : Moy  \n3 : Diff \nNiveau :  ");
+        scanf("%d", &Niveau);
+        if (Niveau == 1)
+        {
+            Max = 100;
+            printf("**[1..100]**\n");
+        }
+        else if (Niveau == 2)
+        {
+            Max = 1000;
+            printf("**[1..1000]**\n");
+        }
+        else 
+        {
+            Max = 10000;
+            printf("**[1..10000]**\n");
+        }
+    } while (Niveau < 1 && Niveau > 3);
+    srand(time(NULL));
+    nombreMystere = (rand() % (Max - Min + 1)) + Min;
+        do
+        {
+            compteur++;
+            printf("Etap %d \n", compteur);
+            printf("Quel est le nombre :");
+            scanf("%d", &nombreEntre);
 
-        if (nombreMystere > nombreEntre)
-        {
-            printf("C'est plus ! \n\n");
-        }
-        else if (nombreMystere < nombreEntre)
-        {
-            printf("C'est mois !\n\n");
-        }
-        else
-        {
-            printf("Tres bien c'est le nombre ! \n\n");
-        }
-    } while (nombreEntre != nombreMystere);
+            if (nombreMystere > nombreEntre)
+            {
+                printf("C'est plus ! \n\n");
+            }
+            else if (nombreMystere < nombreEntre)
+            {
+                printf("C'est mois !\n\n");
+            }
+            else
+            {
+                printf("Tres bien c'est le nombre ! \n\n");
+            }
+        } while (nombreEntre != nombreMystere);
+        printf("Bravo, vous avez trouvé le nombre mystère en %d coups !\n", compteur);
     return 0;
 }
