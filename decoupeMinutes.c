964c6653f90340d81b495bf2afd03786efdee217
Hatem-Ad <45299723+Hatem-Ad@users.noreply.github.com>
2021-03-06 10:40:52 +0100

    Create decoupeMinutes.c

---
+++ decoupeMinutes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void decoupeMinutes(int heurs, int minutes); // fichier.h pas important dans le court programme
+
+int main(int argc , char *argv[])
+{
+    int heurs = 0, minutes = 0;
+    printf("Donner Combient de minutes : ");
+    scanf("%d", &minutes);
+
+    decoupeMinutes(heurs, minutes);
+
+    printf("%d heurs et %d minutes !\n", heurs, minutes);
+    return 0;
+}
+
+void decoupeMinutes(int heurs, int minutes)
+{
+    heurs = minutes / 60;
+    minutes = minutes % 60;
+}
